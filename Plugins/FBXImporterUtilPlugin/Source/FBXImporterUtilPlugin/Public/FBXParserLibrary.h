a743ad98b156dba4696452627cb6d4944ded94b6
孙树超 <1803442018@qq.com>
2025-11-26 13:47:54 +0800

    ADD 创建空的BlueprintFunctionLibrary

---
+++ Plugins/FBXImporterUtilPlugin/Source/FBXImporterUtilPlugin/Public/FBXParserLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FBXParserLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FBXIMPORTERUTILPLUGIN_API UFBXParserLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
